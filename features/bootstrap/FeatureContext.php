d2b5df8ede0d6a83b4940110f4d4d9986ff8d213
miya0001 <miya@wpist.me>
2017-01-08 22:19:12 +0900

    bug fix on behat test

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -179,18 +179,7 @@ class FeatureContext extends BehatContext implements ClosuredContextInterface {
 
 	public function replace_variables( $str ) {
 		$str = preg_replace_callback( '/\{([A-Z_]+)\}/', array( $this, '_replace_var' ), $str );
-		$str = preg_replace_callback( '/\$([A-Z_]+)/', array( $this, '_replace_env_var' ), $str );
 		return $str;
-	}
-
-	private function _replace_env_var( $matches ) {
-		$cmd = $matches[0];
-
-		foreach ( array_slice( $matches, 1 ) as $key ) {
-			$cmd = str_replace( '$' . $key, "'" . getenv( $key ) . "'", $cmd );
-		}
-
-		return $cmd;
 	}
 
 	private function _replace_var( $matches ) {
@@ -260,6 +249,9 @@ class FeatureContext extends BehatContext implements ClosuredContextInterface {
 		} else {
 			$cwd = null;
 		}
+
+		$command = str_replace( '$SHIFTER_USER', getenv( 'SHIFTER_USER' ), $command );
+		$command = str_replace( '$SHIFTER_PASS', getenv( 'SHIFTER_PASS' ), $command );
 
 		return Process::create( $command, $cwd, $env );
 	}
